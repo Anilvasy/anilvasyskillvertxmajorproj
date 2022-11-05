95b107faa77cc75996b6d66671aec0fe09d7ee27
Anilvasy <115612315+Anilvasy@users.noreply.github.com>
2022-11-05 22:18:14 +0530

    Add files via upload

---
+++ food.sql
@@ -0,0 +1,13 @@
+-- phpMyAdmin SQL Dump
+-- version 4.8.4
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Apr 03, 2020 at 11:26 AM
+-- Server version: 10.1.37-MariaDB
+-- PHP Version: 5.6.39
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
